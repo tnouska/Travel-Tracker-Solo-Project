7a3afaa7126555f1078f7e12be9937b3c1452a41
Teagan Nouska <tnouska@live.com>
2018-05-02 09:34:50 -0500

    able to upload .gpx file of track data

--- src/components/TrackPage/TrackPage.js
+++ src/components/TrackPage/TrackPage.js
@@ -5,13 +5,23 @@ import Nav from '../../components/Nav/Nav';
 
 import { USER_ACTIONS } from '../../redux/actions/userActions';
 import { triggerLogout } from '../../redux/actions/loginActions';
+import xml2js from 'xml2js'
 
 
+
+const parseString = xml2js.parseString;
 const mapStateToProps = state => ({
   user: state.user,
 });
 
+
 class TrackPage extends Component {
+  constructor(props){
+    super(props);
+    this.state = {
+      textFile: undefined,
+    }
+  }
   componentDidMount() {
     this.props.dispatch({ type: USER_ACTIONS.FETCH_USER });
   }
@@ -22,6 +32,29 @@ class TrackPage extends Component {
     }
   }
 
+  handleSubmit = (event) => {
+    event.preventDefault(); 
+    let reader = new FileReader();
+    //will set FileReader() function to reader variable
+    reader.readAsText(this.fileInput.files[0]);  
+    //reader will read the file that was uploaded at position 0 and output the text of the file as a string
+    //to reader.result
+    reader.onload = (event) => {
+    //reader.onload will run whenever a reading operation is completed.
+      parseString(reader.result, (error, result) => {
+        if (error) {
+          console.log('error on parseString TrackPage.js', error);
+        } else {
+          console.log(result);
+          // this.props.dispatch({
+          //   type: 'POST_FILE',
+          //   payload: result
+          // });//end dispatch to post uploaded track file
+        };//end if/else statement
+      });//end parseString function
+    };//end reader.onload
+  };//end handleSubmit function
+
   logout = () => {
     this.props.dispatch(triggerLogout());
     // this.props.history.push('home');
@@ -29,6 +62,13 @@ class TrackPage extends Component {
 
   render() {
     let content = null;
+    // let fileContent = null;
+    // if(this.state.textFile) {
+    //   fileContent = this.state.textFile.gpx.wpt.map((waypoint)=>{
+    //     return (<tr key={waypoint.name}><td>{waypoint.name}</td><td>{waypoint._lat}</td><td>{waypoint._lon}</td></tr>) 
+    //   })
+
+    // }
 
     if (this.props.user.userName) {
       content = (
@@ -38,6 +78,19 @@ class TrackPage extends Component {
           >
             Welcome, { this.props.user.userName }!
           </h1>
+          <form onSubmit={this.handleSubmit}>
+            <label>
+              Upload file:
+          <input
+                type="file"
+                ref={input => {
+                  this.fileInput = input;
+                }}
+              />
+            </label>
+            <br />
+            <button type="submit">Submit</button>
+          </form>
           <button
             onClick={this.logout}
           >
@@ -51,6 +104,14 @@ class TrackPage extends Component {
       <div>
         <Nav />
         { content }
+        {/* <table>
+          <thead>
+            <tr><th>name</th><th>lat</th><th>lon</th></tr>
+            </thead>
+          <tbody>
+          {fileContent}
+          </tbody>
+        </table> */}
       </div>
     );
   }
